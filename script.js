39e05a39c6a200ee09136ee08812ba2ed9e7dc98
unknown <divjakdanilo@gmail.com>
2025-03-11 15:30:18 +0100

    prvi

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll(".aboutMe-content p").forEach(p => {
+      p.setAttribute("data-aos", "zoom-in");
+    });
+  });
+
+
+  
